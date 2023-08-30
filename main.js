f6bd0ed94e941ec5be6106a1fbb83bace8c9f042
achrafness <achraf.nessighaoui13@gmail.com>
2023-08-30 14:56:58 +0100

    fix some bugs and add profile page

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-let data= [];
 const input = document.querySelector(".todo-input");
 const btn = document.querySelector(".btn-to");
 const list = document.querySelector(".list");
-
+let data;
+if(localStorage.getItem("list")){
+    data = localStorage.getItem("list").split(',')
+}else{
+    data = ["go to the gym","learn about islam"];
+}
+for(let i in data){
+            text=data[i];
+            let task = document.createElement("li");
+            task.classList.add("mt-10", "bg-blue", "d-block", "rad-6", "p-10", "w-90","d-flex" ,"p-relative");
+            task.textContent = text;
+    
+            let iconElement = document.createElement("i");
+            iconElement.classList.add("fa-sharp", "fa-solid", "fa-circle-xmark", "c-red", "remove");
+            // Append the <i> element to the <li> element
+            task.appendChild(iconElement);
+            // Append the task element in the list
+            list.appendChild(task);
+}
+btn.addEventListener("click",(event)=>{
+        add()
+})
 input.addEventListener("keypress",(event)=>{
     if (event.key === "Enter"){
         add()
     }
 })
 function add()  {
-    if (input.value !== "") {
         let task = document.createElement("li");
         task.classList.add("mt-10", "bg-blue", "d-block", "rad-6", "p-10", "w-90","d-flex" ,"p-relative");
         task.textContent = input.value;
@@ -23,7 +42,6 @@ function add()  {
         data.push(input.value)
         input.value = "";
         localStorage.setItem(`list`, data);
-    }
 }
 
 list.addEventListener("click", (event) => {
